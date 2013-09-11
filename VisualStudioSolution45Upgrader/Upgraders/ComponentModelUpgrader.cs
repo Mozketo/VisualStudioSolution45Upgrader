70abe39199e23f80a34f945bebe4a104ef48cee9
Ben Clark-Robinson <ben.clarkrobinson@gmail.com>
2013-09-11 15:45:01 +1000

    Added componentmodel upgrader - cheers Tyson

--- VisualStudioSolution45Upgrader/Upgraders/ComponentModelUpgrader.cs
+++ VisualStudioSolution45Upgrader/Upgraders/ComponentModelUpgrader.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,25 +9,25 @@ namespace VisualStudioSolution45Upgrader.Upgraders
     /// <summary>
     /// http://www.dotnetexpertguide.com/2011/12/upgrade-aspnet-mvc-3-project-to-mvc-4.html
     /// </summary>
-    public class Mvc4Upgrader : BaseUpgrader, IUpgrader
+    public class ComponentModelUpgrader : BaseUpgrader, IUpgrader
     {
         Dictionary<string, string> upgradeAs = new Dictionary<string, string>();
-        List<string> _searchPattern = new List<string> { "*.csproj", "*.vbproj", "*.fsproj" };
+        List<string> _searchPattern = new List<string> { "*.cs" };
 
-        public Mvc4Upgrader()
+        public ComponentModelUpgrader()
         {
-            upgradeAs.Add("E53F8FEA-EAE0-44A6-8774-FFD645390401", "E3E379DF-F4C6-4180-9B81-6769533ABE47");
-        }
-
-        public List<string> SearchPattern
-        {
-            get { return _searchPattern; }
+            upgradeAs.Add("[Compare(", "[System.ComponentModel.DataAnnotations.Compare(");
         }
 
         public List<string> Upgrade(List<string> lines, out bool didUpgrade)
         {
             lines = base.Upgrade(upgradeAs, lines, out didUpgrade);
             return lines;
+        }
+
+        public List<string> SearchPattern
+        {
+            get { return _searchPattern; }
         }
     }
 }
--- VisualStudioSolution45Upgrader/Upgraders/WebConfigVersionUpgrader.cs
+++ VisualStudioSolution45Upgrader/Upgraders/WebConfigVersionUpgrader.cs
@@ -10,7 +10,7 @@ namespace VisualStudioSolution45Upgrader.Upgraders
     public class WebConfigVersionUpgrader : BaseUpgrader, IUpgrader
     {
         Dictionary<string, string> upgradeFromTo = new Dictionary<string, string>();
-        List<string> _searchPattern;
+        List<string> _searchPattern = new List<string> { "web.config" };
 
         public WebConfigVersionUpgrader()
         {
@@ -19,8 +19,6 @@ namespace VisualStudioSolution45Upgrader.Upgraders
             upgradeFromTo.Add("System.Web.Helpers, Version=1.0.0.0", "System.Web.Helpers, Version=2.0.0.0");
             upgradeFromTo.Add("System.Web.WebPages.Razor, Version=1.0.0.0", "System.Web.WebPages.Razor, Version=2.0.0.0");
             upgradeFromTo.Add("webpages:Version\" value=\"1.0.0.0", "webpages:Version\" value=\"2.0.0.0");
-
-            _searchPattern = new List<string> { "web.config" };
         }
 
         public List<string> SearchPattern
